c470c9e6fdab1acced9985a9ce7dfc0b3771d5e9
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-23 21:18:31 +0530

    Find views

--- app/src/main/java/in/ghostreborn/purchaserreborn/fragment/UpdateProductFragment.java
+++ app/src/main/java/in/ghostreborn/purchaserreborn/fragment/UpdateProductFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,9 +13,17 @@ import in.ghostreborn.purchaserreborn.R;
 
 public class UpdateProductFragment extends Fragment {
 
+    EditText productNameEdit;
+    EditText productPriceEdit;
+    Button productSaveButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_update_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_update_product, container, false);
+        productNameEdit = view.findViewById(R.id.product_name_edit);
+        productPriceEdit = view.findViewById(R.id.product_price_edit);
+        productSaveButton = view.findViewById(R.id.save_product_button);
+        return view;
     }
 }
