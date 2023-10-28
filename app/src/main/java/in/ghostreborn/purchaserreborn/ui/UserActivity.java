73040f0a1d8dd5a97fdcf03c33ab0fa82fd3b82b
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-29 03:55:24 +0530

    List products on user's screen

--- app/src/main/java/in/ghostreborn/purchaserreborn/ui/UserActivity.java
+++ app/src/main/java/in/ghostreborn/purchaserreborn/ui/UserActivity.java
@@ -1,10 +1,14 @@
 package in.ghostreborn.purchaserreborn.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import in.ghostreborn.purchaserreborn.R;
+import in.ghostreborn.purchaserreborn.adapter.ProductAdapter;
+import in.ghostreborn.purchaserreborn.utils.PurchaserHelper;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        PurchaserHelper.setupProducts(this);
+        RecyclerView recyclerView = findViewById(R.id.products_recycler);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        ProductAdapter adapter = new ProductAdapter();
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
     }
 }
