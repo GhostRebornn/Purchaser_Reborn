14543cb433aca0a931c53b0574ca3b73dfed12e2
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-29 03:51:28 +0530

    code Cleanup

--- app/src/main/java/in/ghostreborn/purchaserreborn/utils/PurchaserHelper.java
+++ app/src/main/java/in/ghostreborn/purchaserreborn/utils/PurchaserHelper.java
@@ -12,38 +12,34 @@ import in.ghostreborn.purchaserreborn.database.PurchaserDatabase;
 public class PurchaserHelper {
 
     public static void setupProducts(Context context) {
-        try {
-            try (PurchaserDatabase database = new PurchaserDatabase(context)) {
-                Constants.products = new ArrayList<>();
-                SQLiteDatabase db = database.getReadableDatabase();
-                String query = "SELECT * FROM " + Constants.TABLE_NAME_PRODUCT;
-                Cursor cursor = db.rawQuery(query, null);
-                while (cursor.moveToNext()) {
-                    Constants.products.add(
-                            cursor.getString(1)
-                    );
-                }
-                cursor.close();
+        try (PurchaserDatabase database = new PurchaserDatabase(context)) {
+            Constants.products = new ArrayList<>();
+            SQLiteDatabase db = database.getReadableDatabase();
+            String query = "SELECT * FROM " + Constants.TABLE_NAME_PRODUCT;
+            Cursor cursor = db.rawQuery(query, null);
+            while (cursor.moveToNext()) {
+                Constants.products.add(
+                        cursor.getString(1)
+                );
             }
+            cursor.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void setupUsers(Context context) {
-        try {
-            try (PurchaserDatabase database = new PurchaserDatabase(context)) {
-                Constants.users = new ArrayList<>();
-                SQLiteDatabase db = database.getReadableDatabase();
-                String query = "SELECT * FROM " + Constants.TABLE_NAME_USER;
-                Cursor cursor = db.rawQuery(query, null);
-                while (cursor.moveToNext()) {
-                    Constants.users.add(
-                            cursor.getString(1)
-                    );
-                }
-                cursor.close();
+        try (PurchaserDatabase database = new PurchaserDatabase(context)) {
+            Constants.users = new ArrayList<>();
+            SQLiteDatabase db = database.getReadableDatabase();
+            String query = "SELECT * FROM " + Constants.TABLE_NAME_USER;
+            Cursor cursor = db.rawQuery(query, null);
+            while (cursor.moveToNext()) {
+                Constants.users.add(
+                        cursor.getString(1)
+                );
             }
+            cursor.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
