4ff14d4d5f5a881cb649414981e75b3340e4788d
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-31 03:28:33 +0530

    Test ImagePicker

--- app/src/main/java/in/ghostreborn/purchaserreborn/MainActivity.java
+++ app/src/main/java/in/ghostreborn/purchaserreborn/MainActivity.java
@@ -1,14 +1,44 @@
 package in.ghostreborn.purchaserreborn;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sangcomz.fishbun.FishBun;
+import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 27;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button selectButton = findViewById(R.id.selectButton);
+        selectButton.setOnClickListener(v -> FishBun.with(MainActivity.this)
+                .setImageAdapter(new GlideAdapter())
+                .startAlbumWithOnActivityResult(REQUEST_CODE));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                assert data != null;
+                ArrayList<Uri> uris = data.getParcelableArrayListExtra(FishBun.INTENT_PATH);
+                assert uris != null;
+                Log.e("TAG", uris.get(0).toString());
+            }
+        }
     }
 }
