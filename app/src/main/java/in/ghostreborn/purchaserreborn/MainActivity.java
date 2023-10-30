97b95dacf53728a12d6eae16a64c52d5893bb96a
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-31 03:33:54 +0530

    Show image in ImageView

--- app/src/main/java/in/ghostreborn/purchaserreborn/MainActivity.java
+++ app/src/main/java/in/ghostreborn/purchaserreborn/MainActivity.java
@@ -1,10 +1,14 @@
 package in.ghostreborn.purchaserreborn;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.sangcomz.fishbun.FishBun;
 import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
                 ArrayList<Uri> uris = data.getParcelableArrayListExtra(FishBun.INTENT_PATH);
                 assert uris != null;
                 Log.e("TAG", uris.get(0).toString());
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uris.get(0));
+                    ImageView imageView = findViewById(R.id.imageView);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
